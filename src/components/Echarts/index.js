b4811fc287f2c941bfc93c05c41faa3acec67e2a
小月 <429163339@qq.com>
2018-05-03 16:29:16 +0800

    Update index.js

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -3,40 +3,43 @@ import { WebView, View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 
+let scalesPageToFitValue
+Platform.OS === 'ios' ? (scalesPageToFitValue = false) : (scalesPageToFitValue = true)
+let source = (Platform.OS === 'ios') ? require('./tpl.html'): {'uri':'file:///android_asset/tpl.html'}
 export default class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.setNewOption = this.setNewOption.bind(this);
-  }
-  
-
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.option !== this.props.option) {
-      this.refs.chart.reload();
+    constructor(props) {
+        super(props);
+        this.setNewOption = this.setNewOption.bind(this);
     }
-  }
 
-  setNewOption(option) {
-    this.refs.chart.postMessage(JSON.stringify(option));
-  }
 
-  render() {
-    return (
-      <View style={{flex: 1, height: this.props.height || 400,}}>
-        <WebView
-          ref="chart"
-          scrollEnabled = {false}
-          injectedJavaScript = {renderChart(this.props)}
-          style={{
-            height: this.props.height || 400,
-            backgroundColor: this.props.backgroundColor || 'transparent'
-          }}
-          scalesPageToFit={Platform.OS !== 'ios'}
-          source={require('./tpl.html')}
-          onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
-        />
-      </View>
-    );
-  }
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.option !== this.props.option) {
+            this.refs.chart.reload();
+        }
+    }
+
+    setNewOption(option) {
+        this.refs.chart.postMessage(JSON.stringify(option));
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1, height: this.props.height || 400,}}>
+                <WebView
+                    ref="chart"
+                    scrollEnabled = {false}
+                    injectedJavaScript = {renderChart(this.props)}
+                    style={{
+                        height: this.props.height || 400,
+                        backgroundColor: this.props.backgroundColor || 'transparent'
+                    }}
+                    scalesPageToFit={Platform.OS !== 'ios'}
+                    source={source}
+                    onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
+                />
+            </View>
+        );
+    }
 }
